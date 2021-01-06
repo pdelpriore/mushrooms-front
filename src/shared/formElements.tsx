ed3f3411a3b5f442370dd1c11111a17e0b419315
pdelpriore <paul.del.priore29@gmail.com>
2021-01-06 13:41:27 +0100

    form elements correction

--- src/shared/formElements.tsx
+++ src/shared/formElements.tsx
@@ -1,34 +1,28 @@
-interface SubElements {
+interface Elements {
   question: string;
   answer: string[];
   shortcut: string[];
 }
 
-interface Elements {
-  [key: string]: SubElements;
-}
-
 export const formElements: Elements[] = [
   {
-    capSurface: {
-      question: "cap surface ?",
-      answer: ["fibrous", "grooves", "scaly", "smooth"],
-      shortcut: ["f", "g", "y", "s"],
-    },
-    bruises: {
-      question: "bruises ?",
-      answer: ["bruises", "no"],
-      shortcut: ["t", "f"],
-    },
-    gillSpacing: {
-      question: "gill spacing ?",
-      answer: ["close", "crowded"],
-      shortcut: ["c", "w"],
-    },
-    gillSize: {
-      question: "gill size ?",
-      answer: ["broad", "narrow"],
-      shortcut: ["b", "n"],
-    },
+    question: "cap surface ?",
+    answer: ["fibrous", "grooves", "scaly", "smooth"],
+    shortcut: ["f", "g", "y", "s"],
+  },
+  {
+    question: "bruises ?",
+    answer: ["bruises", "no"],
+    shortcut: ["t", "f"],
+  },
+  {
+    question: "gill spacing ?",
+    answer: ["close", "crowded"],
+    shortcut: ["c", "w"],
+  },
+  {
+    question: "gill size ?",
+    answer: ["broad", "narrow"],
+    shortcut: ["b", "n"],
   },
 ];
