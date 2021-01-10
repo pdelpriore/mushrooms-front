8af4b72773f829aa36b23f945124466257ae18f5
pdelpriore <paul.del.priore29@gmail.com>
2021-01-10 13:55:55 +0100

    set current index to 0 when reset pressed

--- src/views/app/App.tsx
+++ src/views/app/App.tsx
@@ -42,6 +42,7 @@ const App: React.FC = () => {
 
   const handleReset = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
+    setCurrentIndex(0);
     setInputs({});
     // dispatch clearState
   };
